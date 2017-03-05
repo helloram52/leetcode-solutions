8706923a25ab4f706284a9094fdc06803f0c4b20
Ramkumar <helloram52@gmail.com>
2017-03-05 13:27:40 -0600

    Create fourSum.java

---
+++ fourSum.java
@@ -0,0 +1,49 @@
+//18. 4Sum
+
+
+public class fourSum {
+    public List<List<Integer>> fourSum(int[] num, int target) {
+ 
+        ArrayList<List<Integer>> result = new ArrayList<>();
+ 
+        if(num.length<4)return result;
+ 
+        Arrays.sort(num);
+ 
+        for(int i=0; i<num.length-3; i++){
+ 
+            if(i>0 && num[i]==num[i-1])
+              continue;
+ 
+            for(int j=i+1; j<num.length-2; j++){
+ 
+                if(j>i+1 && num[j]==num[j-1])
+                  continue;
+ 
+                int low=j+1, high=num.length-1;
+ 
+                while(low<high){
+ 
+                    int sum=num[i]+num[j]+num[low]+num[high];
+ 
+                    if(sum==target){
+                        
+                        result.add(Arrays.asList(num[i], num[j], num[low], num[high]));
+                        
+                        while(low<high&&num[low]==num[low+1])low++;
+                        
+                        while(low<high&&num[high]==num[high-1])high--;
+                        
+                        low++;
+                        high--;
+                    }
+ 
+                    else if(sum<target)low++;
+ 
+                    else high--;
+                }
+            }
+        }
+        return result;
+    }
+}
